bd7bef2afbbb5c0553e6ff4cde002a3ea3a6624e
Amir <amirkabdou@gmail.com>
2024-08-16 19:16:22 -0700

    Add project files.

---
+++ GenericCFRGameTree/GenericCFRGameTree.cpp
@@ -0,0 +1,12 @@
+// GenericCFRGameTree.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+template <typename A, typename C, typename S>
+void fnGenericCFRGameTree()
+{
+
+}
