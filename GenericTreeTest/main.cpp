64ddf20f4521de55fa3f62419a4230bdc615eaf8
Amir <amirkabdou@gmail.com>
2024-08-26 06:35:50 -0700

    Creating development branch

--- GenericTreeTest/main.cpp
+++ GenericTreeTest/main.cpp
@@ -6,16 +6,10 @@ int main(int argc, char* argv[]) {
 
 	RockPaperScissors* game = new RockPaperScissors();
 	game->CreateGameTree();
-	int result = game->tree->PreProcessor();
-	std::cout << result << "\n";
-
 	
 	game->tree->ConstructTree();
-
-	std::cout << "Before CFR: \n\n";
-	game->tree->PrintGameTree();
 	
-	game->tree->CFR(50, 0);
+	game->tree->CFR(300, 0);
 
 	std::cout << "After CFR: \n\n";
 	game->tree->PrintGameTree();
